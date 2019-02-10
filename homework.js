2f3f6787b1885110b1598cdd264d9a80331bbd3e
Starswordsmen <saltansergei@gmail.com>
2019-02-10 22:38:45 +0200

    Saltan changes

--- homework.js
+++ homework.js
@@ -1,2 +1,66 @@
 // -------------------------- Home work --------------------------
 // -------------------------- ФИО --------------------------
+// // -------------------------- Home work №3 --------------------------
+// // -------------------------- ФИО  Салтан Сергей--------------------------
+// // 1.
+// let str = 'i am in the easycode';
+// let bigStr = " ";
+// for (let i = 0; i < str.length; i++) {
+//     if (str[i - 1] === ' ' || i === 0) {
+//         bigStr += str[i].toUpperCase();
+//     } else if (str[i] !== ' ') {
+//         bigStr += str[i];
+//         console.log(bigStr);
+//     }
+// };
+
+// 2.
+// let string = 'tseb eht ma i';
+// for(let i = string.length; i > -1; i--) {
+//     console.log(string[i]);
+// }
+
+
+// 3. Факториал числа - произведение всех натуральных чисел от 1 до n
+// включительно: 3! = 3*2*1, 5! = 5*4*3*2*1. 
+// С помощью циклов вычислить факториал числа 10. Использовать for
+// let nmb = " ";
+// for (let key of let nmb) {
+//     if (10! = 10*9*8*7*6*5*4*3*2*1 =)
+// }
+
+// 4. 
+// let str = 'JavaScript is a pretty good language';
+// let newStr = '';
+// for(i = 0; i < str.length; i++) {
+//     if (str[ i - 1] === ' '|| i === 0) {
+//         newStr += str[i].toUpperCase();  
+//     } else if (str[i] !== ' ') {
+//                 newStr += str[i]; 
+//                 newStr.split(' ').join('');
+
+//     console.log(newStr);      
+//     }
+// }
+
+// 5. 
+// var myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+// for (let key of myArray) {
+//     if (key % 2 == 0) { continue; }
+//     console.log(key);
+// }
+
+// 6. 
+// let list = {
+//     name: 'denis',
+//     work: 'easycode',
+//     age: 29
+// };
+
+// for (let key in list) {
+//     if (list[key] == ''){
+//         console.log(list[key].toUpperCase);
+//     } else (list[key] == ''){
+//         console.log('Неверное значение');
+//     }
+// }
